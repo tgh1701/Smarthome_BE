b7ae1e43faeef5363c3808ebad5b35d721c57cb0
Hien Truong <tgh1701@gmail.com>
2024-01-05 02:55:17 +0700

    Update MQTT

--- mqtt/mqtt.jsx
+++ mqtt/mqtt.jsx
@@ -1,7 +1,9 @@
 const mqtt = require("mqtt");
 
 const options = {
-  host: "192.168.0.119",
+  host: "192.168.0.119", // PC
+  // host: "192.168.0.154", // MAC
+  // host: "172.20.10.2", // MAC with Iphone
   port: 1883,
 };
 const mqttClient = mqtt.connect(options);
